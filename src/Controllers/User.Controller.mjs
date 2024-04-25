fe0cf0181b9164082b11964d94507e86df30acf8
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-04-25 02:17:55 +0200

    feat: Implemented user management endpoints for admin functionality

--- src/Controllers/User.Controller.mjs
+++ src/Controllers/User.Controller.mjs
@@ -1,9 +1,10 @@
-import { UserModel } from "../Model/User.Model.mjs";
-import { VerificationModel } from "../Model/Verification.Model.mjs";
+import { UserModel } from '../Model/User.Model.mjs';
+import { VerificationModel } from '../Model/Verification.Model.mjs';
+import { catchAsync } from '../utils/catchAsync.mjs';
 
 const profile = (req, res) => {
   const user = req.decodedUser;
-  return res.status(200).json({ Name: "Profile", Users: user });
+  return res.status(200).json({ Name: 'Profile', Users: user });
 };
 
 const verify = async (req, res) => {
@@ -11,8 +12,8 @@ const verify = async (req, res) => {
   try {
     const user = await UserModel.findOne({ _id: id });
     if (!user) {
-      console.log("No User");
-      return res.status(400).json({ Status: 400, Error: "Invalid Link" });
+      console.log('No User');
+      return res.status(400).json({ Status: 400, Error: 'Invalid Link' });
     }
 
     const uid = await VerificationModel.findOne({
@@ -21,14 +22,14 @@ const verify = async (req, res) => {
     });
 
     if (!uid) {
-      console.log("No UUID");
-      return res.status(400).json({ Status: 400, Error: "Invalid Link" });
+      console.log('No UUID');
+      return res.status(400).json({ Status: 400, Error: 'Invalid Link' });
     }
 
     const currentDate = new Date();
 
     if (uid.expireAt > currentDate) {
-      return res.status(400).json({ status: 400, Error: "Link Expired" });
+      return res.status(400).json({ status: 400, Error: 'Link Expired' });
     }
 
     const verified = await UserModel.updateOne({
@@ -40,14 +41,71 @@ const verify = async (req, res) => {
     if (verified) {
       return res
         .status(200)
-        .json({ Status: 200, Msg: "User Verified Successfully" });
+        .json({ Status: 200, Msg: 'User Verified Successfully' });
     }
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ Status: 400, Error: "Error Occured" });
+    return res.status(400).json({ Status: 400, Error: 'Error Occured' });
   }
 };
 
-const UserController = { profile, verify };
+// Admin
+const deleteUser = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const user = await UserModel.findByIdAndDelete(id);
+  if (!user) {
+    return res.status(400).json({ Status: 400, error: 'User Not Found' });
+  }
+  return res.status(200).json({ Status: 200, message: 'User Deleted' });
+});
+
+const getUsers = catchAsync(async (req, res) => {
+  const users = await UserModel.aggregate([
+    {
+      $match: {
+        role: 'user',
+      },
+    },
+  ]);
+  if (!users) {
+    return res.status(400).json({ Status: 400, error: 'No Users Found' });
+  }
+  return res.status(200).json({ Status: 200, Users: users });
+});
+
+const getUser = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const user = await UserModel.findById(id);
+  // .populate('orders')
+  // .populate('cart')
+  // .exec();
+  if (!user) {
+    return res.status(400).json({ Status: 400, error: 'User Not Found' });
+  }
+  return res.status(200).json({ Status: 200, User: user });
+});
+
+const countUsers = catchAsync(async (req, res) => {
+  const count = await UserModel.aggregate([
+    {
+      $match: {
+        role: 'user',
+      },
+    },
+    {
+      $count: 'users',
+    },
+  ]);
+  return res.status(200).json({ count });
+});
+
+const UserController = {
+  profile,
+  verify,
+  deleteUser,
+  getUsers,
+  getUser,
+  countUsers,
+};
 
 export { UserController };
--- src/Routes/User.Routes.mjs
+++ src/Routes/User.Routes.mjs
@@ -1,10 +1,19 @@
-import express from "express";
-import { checkUser, requireAuth } from "../Middleware/Auth.Middleware.mjs";
-import { UserController } from "../Controllers/User.Controller.mjs";
+import express from 'express';
+import { checkUser, requireAuth } from '../Middleware/Auth.Middleware.mjs';
+import { UserController } from '../Controllers/User.Controller.mjs';
 const UserRoutes = express.Router();
 
-UserRoutes.get("/profile", requireAuth, UserController.profile);
+UserRoutes.get('/profile', requireAuth, UserController.profile);
 
-UserRoutes.get("/verify/:id/:uuid", UserController.verify);
+UserRoutes.get('/verify/:id/:uuid', UserController.verify);
+
+// Admin
+UserRoutes.get('/count', UserController.countUsers);
+
+UserRoutes.delete('/:id',requireAuth, UserController.deleteUser);
+
+UserRoutes.get('/',requireAuth, UserController.getUsers);
+
+UserRoutes.get('/:id',requireAuth, UserController.getUser);
 
 export { UserRoutes };

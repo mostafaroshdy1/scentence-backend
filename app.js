5696e406670b6b816ae8cc071a037962bef587cf
Mostafa Elgreatly <66712535+mostafaroshdy1@users.noreply.github.com>
2024-05-27 04:23:06 +0300

    fix: stipe webhook cors issue

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ const corsOptions = {
   },
 };
 
-app.use(cors(corsOptions));
 app.use("/webhooks", webhookRoutes);
+app.use(cors(corsOptions));
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
